e934d2fb6335214384a2437c620eeb8dcd3cd9c8
Durgesh Yadav <durgeshy5226@gmail.com>
2023-03-30 11:59:39 +0530

    First Commit

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const filterData = [
+  {
+    id: "1",
+    title: "All",
+  },
+  {
+    id: "2",
+    title: "Development",
+  },
+  {
+    id: "3",
+    title: "Business",
+  },
+  {
+    id: "4",
+    title: "Design",
+  },
+  {
+    id: "5",
+    title: "Lifestyle",
+  },
+];
+
+// Data inside above filter is rendered from this below api link i.e api call
+
+export const apiUrl = "https://codehelp-apis.vercel.app/api/get-top-courses";
